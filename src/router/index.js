8732c59f14b89bd1d6cc97a2f7eb169c7750175b
Gabriel Heinrich <gabriel@codecampleipzig.de>
2020-05-06 12:16:55 +0200

    Add empty dashboard and create event components

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,22 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import Dashboard from "@/views/Dashboard.vue";
+import CreateEvent from "@/views/CreateEvent.vue";
 
 Vue.use(VueRouter);
 
-const routes = [];
+const routes = [
+  {
+    path: "/",
+    name: "dashboard",
+    component: Dashboard
+  },
+  {
+    path: "/events/new",
+    name: "create-event",
+    component: CreateEvent
+  }
+];
 
 const router = new VueRouter({
   mode: "history",
