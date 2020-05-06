1f70fc209cc8e73d52b4e76ac68e2b9d23bbf7a4
Gabriel Heinrich <gabriel@codecampleipzig.de>
2020-05-06 12:19:11 +0200

    Setup Vue

---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Dashboard from "@/views/Dashboard.vue";
+import CreateEvent from "@/views/CreateEvent.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "dashboard",
+    component: Dashboard
+  },
+  {
+    path: "/events/new",
+    name: "create-event",
+    component: CreateEvent
+  }
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes
+});
+
+export default router;
