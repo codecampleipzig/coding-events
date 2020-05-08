7c9b85fb5de26fa4fb0655b3882a6f47a4dc7a0a
Gabriel Heinrich <gabriel@codecampleipzig.de>
2020-05-08 10:17:53 +0200

    Add unit tests for Create Event component

--- tests/unit/CreateEvent.spec.js
+++ tests/unit/CreateEvent.spec.js
@@ -8,5 +8,8 @@ describe("CreateEvent", () => {
     expect(wrapper.contains("h1")).toBe(true);
   });
 
-  // it should have a heading saying Create an Event
+  test("it has an <h1> heading with a Create an Event text content", () => {
+    const wrapper = mount(CreateEvent);
+    expect(wrapper.get("h1").text()).toBe("Create an Event");
+  });
 });
