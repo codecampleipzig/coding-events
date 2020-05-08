3ca0bf2d3f1a20c7d116398575156c6ebb4e8ddf
Gabriel Heinrich <gabriel@codecampleipzig.de>
2020-05-08 11:51:12 +0200

    Create input form scaffold for Create Event Page

--- src/views/CreateEvent.vue
+++ src/views/CreateEvent.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
     <h1>Create an Event</h1>
+    <form>
+      <input type="text" name="title" placeholder="Add a Title" />
+      <input type="submit" value="Create" />
+    </form>
   </div>
 </template>
 
--- tests/unit/CreateEvent.spec.js
+++ tests/unit/CreateEvent.spec.js
@@ -10,6 +10,33 @@ describe("CreateEvent", () => {
 
   test("it has an <h1> heading with a Create an Event text content", () => {
     const wrapper = mount(CreateEvent);
+
     expect(wrapper.get("h1").text()).toBe("Create an Event");
+  });
+
+  test("it contains a form element", () => {
+    const wrapper = mount(CreateEvent);
+
+    expect(wrapper.contains("form")).toBe(true);
+  });
+
+  test("it should contain a title input field", () => {
+    const wrapper = mount(CreateEvent);
+
+    expect(wrapper.contains("input[name='title'][type='text']")).toBe(true);
+  });
+
+  test("it should contain a submit button with the value Create", () => {
+    const wrapper = mount(CreateEvent);
+
+    expect(wrapper.contains("input[value='Create'][type='submit']")).toBe(true);
+  });
+
+  test("it should contain an input field for the title with the placeholder 'Add a Title'", () => {
+    const wrapper = mount(CreateEvent);
+
+    const titleInput = wrapper.get("input[name='title']");
+
+    expect(titleInput.attributes("placeholder")).toBe("Add a Title");
   });
 });
