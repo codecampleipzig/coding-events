73c80aac019970a37b9e027bb77ca8e7746056e9
Gabriel Heinrich <gabriel@codecampleipzig.de>
2020-05-08 12:11:48 +0200

    Add event property and two-way binding to title input field

--- src/views/CreateEvent.vue
+++ src/views/CreateEvent.vue
@@ -2,14 +2,27 @@
   <div>
     <h1>Create an Event</h1>
     <form>
-      <input type="text" name="title" placeholder="Add a Title" />
+      <input
+        v-model="event.title"
+        type="text"
+        name="title"
+        placeholder="Add a Title"
+      />
       <input type="submit" value="Create" />
     </form>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      event: {
+        title: ""
+      }
+    };
+  }
+};
 </script>
 
 <style scoped></style>
--- tests/unit/CreateEvent.spec.js
+++ tests/unit/CreateEvent.spec.js
@@ -39,4 +39,21 @@ describe("CreateEvent", () => {
 
     expect(titleInput.attributes("placeholder")).toBe("Add a Title");
   });
+
+  test("it should have an event data property", () => {
+    const wrapper = mount(CreateEvent);
+
+    expect(wrapper.vm.event).toEqual({
+      title: ""
+    });
+  });
+  test("it should bind the event title to the user input", () => {
+    const wrapper = mount(CreateEvent);
+
+    const titleInput = wrapper.get("input[name='title']");
+
+    titleInput.setValue("JS Pair Programming Session");
+
+    expect(wrapper.vm.event.title).toBe("JS Pair Programming Session");
+  });
 });
