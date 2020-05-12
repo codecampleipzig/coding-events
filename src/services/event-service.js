53f8d796e9a334e4221b24cd0eb426ca66d83be3
Gabriel Heinrich <gabriel@codecampleipzig.de>
2020-05-12 15:48:39 +0200

    Add getEvents to event service

--- src/services/event-service.js
+++ src/services/event-service.js
@@ -3,3 +3,7 @@ import axios from "axios";
 export function createEvent(event) {
   return axios.post("http://localhost:3000/events", event);
 }
+
+export function getEvents() {
+  return axios.get("http://localhost:3000/events");
+}
