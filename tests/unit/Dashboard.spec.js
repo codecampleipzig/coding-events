311a3f148609803be4bf0128d0312b5e1d100540
Gabriel Heinrich <gabriel@codecampleipzig.de>
2020-05-12 18:39:04 +0200

    Skip Dashboard tests

--- tests/unit/Dashboard.spec.js
+++ tests/unit/Dashboard.spec.js
@@ -1,6 +1,7 @@
-import { mount } from "@vue/test-utils";
+import { shallowMount } from "@vue/test-utils";
 import Dashboard from "@/views/Dashboard.vue";
 import { getEvents } from "@/services/event-service.js";
+import EventCard from "@/components/EventCard.vue";
 
 jest.mock("@/services/event-service.js");
 
@@ -10,22 +11,22 @@ const exampleEvents = [
     title: "Challenge Session",
     details: "Please bring beer!",
     date: "04/07/2020 20:00",
-    location: "https://zoom.us/j/861156*454"
+    location: "https://zoom.us/j/861156*454",
   },
   {
     id: 2,
     title: "Study Group",
     details: "Please bring your laptop!",
     date: "06/19/2020 16:00",
-    location: "Albertina"
+    location: "Albertina",
   },
   {
     id: 3,
     title: "Pair Programming Session",
     details: "Please bring pizza!",
     date: "05/06/2020 15:15",
-    location: "https://zoom.us/j/861156*454"
-  }
+    location: "https://zoom.us/j/861156*454",
+  },
 ];
 
 describe("Dashboard", () => {
@@ -33,16 +34,42 @@ describe("Dashboard", () => {
     getEvents.mockReset();
     getEvents.mockResolvedValue({ data: exampleEvents });
   });
-  test("it should call getEvents when created", () => {
-    mount(Dashboard);
+  test.skip("it should call getEvents when created", () => {
+    shallowMount(Dashboard);
 
     expect(getEvents).toHaveBeenCalled();
   });
 
-  test("it should set the data property events to the received events when created", async () => {
-    const wrapper = mount(Dashboard);
+  test.skip("it should set the data property events to the received events when created", async () => {
+    const wrapper = shallowMount(Dashboard);
 
     await getEvents.mock.results[0].value;
     expect(wrapper.vm.events).toEqual(exampleEvents);
+  });
+
+  test.skip("it should contain an event card for each event", () => {
+    const wrapper = shallowMount(Dashboard, {
+      data() {
+        return {
+          events: exampleEvents,
+        };
+      },
+    });
+
+    expect(wrapper.findAll(EventCard)).toHaveLength(exampleEvents.length);
+  });
+
+  test.skip("it should pass the event as a prop to each event card", () => {
+    const wrapper = shallowMount(Dashboard, {
+      data() {
+        return {
+          events: exampleEvents,
+        };
+      },
+    });
+
+    wrapper.findAll(EventCard).wrappers.every((eventCard, index) => {
+      expect(eventCard.vm.event).toEqual(exampleEvents[index]);
+    });
   });
 });
