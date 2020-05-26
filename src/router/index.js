004d9c11ad72c290c39af757adb89b0efd00ec92
Gabriel Heinrich <gabriel@codecampleipzig.de>
2020-05-26 11:07:17 +0200

    Add .env and Login View

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Dashboard from "@/views/Dashboard.vue";
 import CreateEvent from "@/views/CreateEvent.vue";
+import Login from "@/views/Login.vue";
 
 Vue.use(VueRouter);
 
@@ -15,6 +16,11 @@ const routes = [
     path: "/events/new",
     name: "create-event",
     component: CreateEvent,
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: Login,
   },
 ];
 
